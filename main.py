2bef0b874a09566c80d08055f195f2591febd90c
Remsy2027 <85223124+Remsy2027@users.noreply.github.com>
2023-07-21 09:07:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import ssl
+import queue
+import threading
 from flask import Flask, request, jsonify
 import subprocess
 import os
@@ -10,6 +12,28 @@ CORS(app, origins=["*"])
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain('/home/remsy/certificate.pem')
+
+# Create a queue to store incoming requests
+request_queue = queue.Queue()
+
+def render_worker():
+    while True:
+        # Get the next request from the queue (blocks if the queue is empty)
+        email, glb_file = request_queue.get()
+        file_path = os.path.join('Assets', f'{email}.glb')
+        glb_file.save(file_path)
+
+        # Your code to process the GLB data or trigger other actions based on the email and GLB data
+        # For example, you can call a function or execute a script to start rendering
+        subprocess.Popen(['bash', 'Render.sh', email])
+
+        # Mark the task as done in the queue
+        request_queue.task_done()
+
+# Start the worker thread to process requests
+worker_thread = threading.Thread(target=render_worker)
+worker_thread.daemon = True
+worker_thread.start()
 
 @app.route('/', methods=['GET'])
 def index():
@@ -23,18 +47,10 @@ def upload_glb():
     if not email or not glb_file:
         return jsonify({'message': 'Email or GLB file not provided'}), 400
 
-    # Save the GLB file to a folder on the server
-    file_path = os.path.join('Assets', f'{email}.glb')
-    glb_file.save(file_path)
+    # Add the request to the queue for processing by the worker thread
+    request_queue.put((email, glb_file))
 
-    # Check if the GLB file is received in the Assets folder and start rendering
-    if os.path.exists(file_path):
-        # Your code to trigger the rendering process using the email and GLB data
-        # For example, you can call a function or execute a script to start rendering
-        subprocess.Popen(['bash', 'Render.sh', email])
-        return jsonify({'message': 'GLB file received and rendering started'}), 200
-    else:
-        return jsonify({'message': 'Failed to save GLB file'}), 500
+    return jsonify({'message': 'GLB file received and added to the queue for rendering'}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, ssl_context=context)
